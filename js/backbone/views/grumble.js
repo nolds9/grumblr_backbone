c0681efc27cc337653e381922acbc60d72adde44
Andy <andrew.sunglae.kim@gmail.com>
2015-08-25 09:24:19 -0400

    add edit form to grumble view

--- js/backbone/views/grumble.js
+++ js/backbone/views/grumble.js
@@ -5,10 +5,15 @@ GrumbleView = Backbone.View.extend({
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
     this.template = Handlebars.compile($("#grumbleTemplate").html());
+    this.editTemplate = Handlebars.compile($("#grumbleFormTemplate").html())
     this.render();
   },
 
   render: function() {
+    var self = this
     this.$el.html(this.template(this.model.toJSON()));
+    this.$el.find(".editButton").on("click", function(){
+      self.$el.html(self.editTemplate(self.model.toJSON()))
+    })
   }
 });
